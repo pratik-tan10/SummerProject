c6d62e3f278d1e69b65fa194ed6282e28c410b49
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-12 22:13:26 -0500

    Update webscrape.R

--- webscrape.R
+++ webscrape.R
@@ -6,33 +6,6 @@ hdoc <- read_html(wlink)
 hdoc%>%html_nodes("tbody:nth-child(1)")%>%html_text()->wtext
 wtext[2]->w2
 
-cleanr <- function(xw){
-  gsub("\n\n",",", xw)->w2
-  gsub("\n",",", w2)->w2
-  w2
-  unlist(strsplit(w2, split = ",")) -> uw2
-  uw2[9:length(uw2)][nchar(uw2)>10]->ulw2
-  ulw2[!is.na(ulw2)][nchar(ulw2)>2]
-  ulw2[grep("–", ulw2)]
-}
-
-unlist(lapply(wtext[2:length(wtext)], cleanr))
-
-unlist(strsplit(wtext[2], split = "\n\n\n"))->t
-gsub("\n\n",",", t) -> t
-trimws(t) -> t
-gsub("\n",",", t)->t
-
-strsplit(t, split = ",")->splited
-max(unlist(lapply(splited, length)))->maxl
-
-sapply( splited, function(x){
-  a = rep("",maxl)
-  a[1:length(x)]<-unlist(x)
-  a
-})->nameMat
-data.frame(t(nameMat))
-
 ##
 extractname <- function(xyz){
   unlist(strsplit(xyz, split = "\n\n\n"))->t
@@ -59,4 +32,29 @@ idf <- multiDF[[1]]
 for ( i in 2:length(multiDF)) {
   idf <- rbind(idf, multiDF[[i]])
 }
-idf[nchar(idf[,1])>10,1:3]
+idf[nchar(idf[,1])>10,1:3] -> df2clean
+
+colnames(df2clean) <- c("Personal", "Start", "End")
+
+personal <- df2clean$Personal
+
+library(stringr)
+personal%>%str_split(pattern = "(?:\\(|\\))") ->personList
+sapply(personList, length)==3 ->selector
+sapply(personList[selector],
+       function(x){
+         x[[1]][1:2]
+       })
+
+data.frame(t(unlist(personList[2])[1:2]))
+m <- length(personList)
+matrix(rep(NA, 2*m), nrow = m, ncol = 2) -> nameMat
+
+for (i in 1:length(personList)){
+  if (length(personList[[i]])==3){
+    nameMat[i,] <- unlist(personList[[i]])[1:2]
+
+  }
+}
+data.frame(nameMat, colnames = c("Prime Minister", "Birth-Death"))
+
